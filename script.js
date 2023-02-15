5e01bfc14b98f20db5ec6e14a36d06946f8da262
Robert Fydrych <fydrych.robert.w@gmail.com>
2023-02-15 23:30:35 +0000

    function added

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let heightElement = document.querySelector(".js-height");
-let weightElement = document.querySelector(".js-weight");
-let formElement = document.querySelector(".js-form");
-let bmiElement = document.querySelector(".js-bmi");
+{
+    let heightElement = document.querySelector(".js-height");
+    let weightElement = document.querySelector(".js-weight");
+    let formElement = document.querySelector(".js-form");
+    let bmiElement = document.querySelector(".js-bmi");
 
-formElement.addEventListener("input", () => {
-    let height = heightElement.value;
-    let weight = weightElement.value;
+    const inputDataOn = () => {
+        let height = heightElement.value;
+        let weight = weightElement.value;
+        let bmi = weight / ((height / 100) ** 2);
+        bmiElement.innerText = bmi.toFixed(2);
+        console.log(height, weight, bmi);
+    }
 
-    let bmi = weight / ((height / 100) ** 2); //*dzielimy przez 100 bo wpisujemy cm a potrzeba metry, height * height to to samo co height ** 2 (potega)*//
-
-    bmiElement.innerText = bmi.toFixed(2); //*toFixed - ilosc liczb po przecinku*//
-
-    console.log(height, weight, bmi);
-})
-
+    formElement.addEventListener("input", inputDataOn);
+}
 //to jest przyklad INPUT//
