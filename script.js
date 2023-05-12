80b5fff1003efbfdbde6a4d21af321443a3d6f4a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-05-12 23:37:24 +0100

    new result added

--- script.js
+++ script.js
@@ -9,6 +9,17 @@
         let weight = weightElement.value;
         let bmi = weight / ((height / 100) ** 2);
         bmiElement.innerText = bmi.toFixed(2);
+
+
+        if (bmi < 18.5) {
+            bmiElement.className = "js-bmi underweight";
+        } else if (bmi > 18.5 && bmi <= 24.9) {
+            bmiElement.className = "js-bmi normal";
+        } else if (bmi > 24.9 && bmi <= 30) {
+            bmiElement.className = "js-bmi overweight underweight";
+        } else {
+            bmiElement.className = "js-bmi obesity";
+        }
     }
 
     formElement.addEventListener("input", inputDataOn);
