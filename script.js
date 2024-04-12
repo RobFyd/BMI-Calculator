190a0f0983a532a055b6de3d99c6f889fa77f945
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-12 23:50:34 +0100

    inputSataOn function added

--- script.js
+++ script.js
@@ -4,4 +4,23 @@
     let weightElement = document.querySelector(".js-weight");
     let formElement = document.querySelector(".js-form");
     let bmiElement = document.querySelector(".js-bmi");
+    const inputDataOn = () => {
+        let height = parseFloat(heightElement.value);
+        let weight = parseFloat(weightElement.value);
+        let bmi = weight / Math.pow((height / 100), 2);
+        bmiElement.innerText = bmi.toFixed(2);
+        if (bmi < 18.5) {
+            bmiElement.className = "js-bmi underweight";
+        }
+        else if (bmi > 18.5 && bmi <= 24.9) {
+            bmiElement.className = "js-bmi normal";
+        }
+        else if (bmi > 24.9 && bmi <= 30) {
+            bmiElement.className = "js-bmi overweight underweight";
+        }
+        else {
+            bmiElement.className = "js-bmi obesity";
+        }
+    };
+    formElement.addEventListener("input", inputDataOn);
 }
--- script.ts
+++ script.ts
@@ -1,7 +1,25 @@
 {
-  let heightElement = document.querySelector(".js-height")as HTMLInputElement;
-  let weightElement = document.querySelector(".js-weight")as HTMLInputElement;
-  let formElement = document.querySelector(".js-form")as HTMLFormElement;
-  let bmiElement = document.querySelector(".js-bmi")as HTMLParagraphElement;
+  let heightElement = document.querySelector(".js-height") as HTMLInputElement;
+  let weightElement = document.querySelector(".js-weight") as HTMLInputElement;
+  let formElement = document.querySelector(".js-form") as HTMLFormElement;
+  let bmiElement = document.querySelector(".js-bmi") as HTMLParagraphElement;
 
+  const inputDataOn = () => {
+    let height = parseFloat(heightElement.value);
+    let weight = parseFloat(weightElement.value);
+    let bmi = weight / (height / 100) ** 2;
+    bmiElement.innerText = bmi.toFixed(2);
+
+    if (bmi < 18.5) {
+      bmiElement.className = "js-bmi underweight";
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+      bmiElement.className = "js-bmi normal";
+    } else if (bmi > 24.9 && bmi <= 30) {
+      bmiElement.className = "js-bmi overweight underweight";
+    } else {
+      bmiElement.className = "js-bmi obesity";
+    }
+  };
+
+  formElement.addEventListener("input", inputDataOn);
 }
