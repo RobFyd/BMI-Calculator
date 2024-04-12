36742e325582690363be8d93a16962f2614b0dc7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-12 23:38:25 +0100

    querySelectors added

---
+++ script.ts
@@ -0,0 +1,7 @@
+{
+  let heightElement = document.querySelector(".js-height")as HTMLInputElement;
+  let weightElement = document.querySelector(".js-weight")as HTMLInputElement;
+  let formElement = document.querySelector(".js-form")as HTMLFormElement;
+  let bmiElement = document.querySelector(".js-bmi")as HTMLParagraphElement;
+
+}
